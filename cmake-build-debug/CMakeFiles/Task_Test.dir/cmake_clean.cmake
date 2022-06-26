86e99b837730756c1e5796864dbedb6d871bcb82
TNTrol <sem.go@bk.ru>
2022-06-26 23:35:48 +0300

    add method of find all point in sector

---
+++ cmake-build-debug/CMakeFiles/Task_Test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task_Test.dir/main.cpp.o"
+  "Task_Test"
+  "Task_Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
